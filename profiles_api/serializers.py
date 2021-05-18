8187d7705e070e0cdd0404116399c37705fccc45
Michal <micmey9@gmail.com>
2021-05-18 20:27:08 +0200

    Added user profile feed API

--- profiles_api/serializers.py
+++ profiles_api/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import UserProfile
+from .models import UserProfile, ProfileFeedItem
 
 
 class HelloSerializer(serializers.Serializer):
@@ -20,3 +20,12 @@ class UserProfileSerializer(serializers.ModelSerializer):
         user = UserProfile.objects.create_user(email=validated_data["email"], name=validated_data["name"], password=validated_data["password"])
 
         return user
+
+
+class ProfileFeedItemSerializer(serializers.ModelSerializer):
+    """Serializes profile feed items"""
+
+    class Meta:
+        model = ProfileFeedItem
+        fields = ("id", "user_profile", "status_text", "created_on")
+        extra_kwargs = {"user_profile": {"read_only":True}}
--- profiles_api/views.py
+++ profiles_api/views.py
@@ -6,9 +6,10 @@ from rest_framework.authentication import TokenAuthentication
 from rest_framework import filters
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
-from .serializers import HelloSerializer, UserProfileSerializer
-from .models import UserProfile
-from .permissions import UpdateOwnProfile
+from rest_framework.permissions import IsAuthenticated
+from .serializers import HelloSerializer, UserProfileSerializer, ProfileFeedItemSerializer
+from .models import UserProfile, ProfileFeedItem
+from .permissions import UpdateOwnProfile, UpdateOwnStatus
 
 
 class HelloApiView(APIView):
@@ -100,3 +101,15 @@ class UserProfileViewSet(ModelViewSet):
 class UserLoginApiView(ObtainAuthToken):
     """Handle creating user authentication tokens"""
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
+
+class UserProfileFeedViewSet(ModelViewSet):
+    """Handles creating, reading and updating profile feed items"""
+    authentication_classes = (TokenAuthentication, )
+    serializer_class = ProfileFeedItemSerializer
+    queryset = ProfileFeedItem.objects.all()
+    permission_classes = (UpdateOwnStatus, IsAuthenticated)
+
+    def perform_create(self, serializer):
+        """Sets the user profile to the logged in user"""
+        serializer.save(user_profile=self.request.user)
